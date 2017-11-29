ba53de1d846ecc73cd608c15f2f41c72eb4af38b
Paul Suda <paulsuda@gmail.com>
2017-11-28 21:40:35 -0600

    Day 1!

--- src/20171129/script.js
+++ src/20171129/script.js
@@ -4,33 +4,50 @@ const THREE = require('three');
 function initRenderCanvas(rootEl){
   const w = window.innerWidth;
   const h = window.innerHeight;
-  renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( w, h );
   rootEl.appendChild( renderer.domElement );
   return [w, h, renderer];
 }
 
-
-
 function main(rootEl) {
   console.log('hello world', rootEl);
   const [w, h, renderer] = initRenderCanvas(rootEl);
-  camera = new THREE.PerspectiveCamera( 70, w / h, 1, 1000 );
+  const camera = new THREE.PerspectiveCamera( 70, w / h, 1, 1000 );
 	camera.position.z = 400;
-	scene = new THREE.Scene();
-	var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
+	const scene = new THREE.Scene();
 	var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
-	var material = new THREE.MeshBasicMaterial( { map: texture } );
-	mesh = new THREE.Mesh( geometry, material );
+  var material = new THREE.MeshPhongMaterial( {
+    color: 0xcc3333,
+    specular: 0xffffff,
+    shininess: 50
+  }  );
+  {
+    const light = new THREE.PointLight( 0xcccccc, 2, 1500 );
+    light.position.set( 200, 250, 300 );
+    scene.add( light );
+  }
+  {
+    const light = new THREE.PointLight( 0x333333, 2, 700 );
+    light.position.set( -600, -150, -100 );
+    scene.add( light );
+  }
+  {
+    const light = new THREE.PointLight( 0x333333, 2, 900 );
+    light.position.set( 300, -850, 100 );
+    scene.add( light );
+  }
+	const mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
 
-  function animate() {
-  	requestAnimationFrame( animate );
-  	mesh.rotation.x += 0.005;
-  	mesh.rotation.y += 0.01;
-  	renderer.render( scene, camera );
-  }
+  const animate = () => {
+    requestAnimationFrame( animate );
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.01;
+    renderer.render( scene, camera );
+  };
+  animate();
 }
 
 module.exports = main;
