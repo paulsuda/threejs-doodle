bc630974745f6f75b1c24cee13f99a23d7e139a8
Paul Suda <paulsuda@gmail.com>
2017-11-28 21:40:23 -0600

    linter tuning

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,11 @@
 module.exports = {
     "env": {
+        "node" : true,
         "browser": true,
         "es6": true
     },
     "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
         "linebreak-style": [
             "error",
             "unix"
