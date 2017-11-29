794cc1bf9eebffbe97a0893d6598fa2d14950360
Paul Suda <paulsuda@gmail.com>
2017-11-28 22:53:48 -0600

    Module switching

--- src/script.js
+++ src/script.js
@@ -5,9 +5,31 @@ const moduleList = [
   require('./20171129-2/script'),
 ];
 
-function main(rootEl) {
-  const viewModuleMain = moduleList[0];
+function nextListen(nextFn){
+  document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log(keyName);
+    nextFn();
+  }, false);
+}
+
+function showIndex(rootEl, i){
+  const viewModuleMain = moduleList[i];
+  rootEl.innerHTML = '';
   return viewModuleMain(rootEl);
+}
+
+function main(rootEl) {
+  let currentIndex = 0;
+  const moduleCount = moduleList.length;
+  showIndex(rootEl, currentIndex);
+  nextListen(function(){
+    currentIndex += 1;
+    if(currentIndex >= moduleCount){
+      currentIndex = 0;
+    }
+    showIndex(rootEl, currentIndex);
+  });
 }
 
 module.exports = main;
