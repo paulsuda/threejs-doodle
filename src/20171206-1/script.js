d6c1eba053130d5cafe8d70226d818d8a0e6aeec
Paul Suda <paulsuda@gmail.com>
2017-12-08 00:38:41 -0600

    Fiddling with text

--- src/20171206-1/script.js
+++ src/20171206-1/script.js
@@ -4,6 +4,58 @@ const { initRenderCanvas } = require('../util');
 
 THREE.OrbitControls = require('./OrbitControls');
 
+function initLineText(shapes, material, translateX){
+  var lineText = new THREE.Object3D();
+  for ( var i = 0; i < shapes.length; i ++ ) {
+    var shape = shapes[ i ];
+    var points = shape.getPoints();
+    var geometry = new THREE.BufferGeometry().setFromPoints( points );
+    var lineMesh = new THREE.Line( geometry, material );
+    geometry.translate( translateX, 0, 0 );
+    lineText.add( lineMesh );
+  }
+  return lineText;
+}
+
+function initHoleShapes(shapes){
+  var holeShapes = [];
+  for ( var i = 0; i < shapes.length; i ++ ) {
+    var shape = shapes[ i ];
+    if ( shape.holes && shape.holes.length > 0 ) {
+      for ( var j = 0; j < shape.holes.length; j ++ ) {
+        var hole = shape.holes[ j ];
+        holeShapes.push( hole );
+      }
+    }
+  }
+  return holeShapes;
+}
+
+function initTextShape(font, message){
+  var textShape = new THREE.BufferGeometry();
+  var shapes = font.generateShapes( message, 100, 2 );
+  var geometry = new THREE.ShapeGeometry( shapes );
+  geometry.computeBoundingBox(); // TODO maybe not needed?
+  var translateX = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+  geometry.translate( translateX, 0, 0 );
+  textShape.fromGeometry( geometry );
+  return [shapes, textShape, translateX];
+}
+
+function initMaterials(){
+  var matDark = new THREE.LineBasicMaterial( {
+    color: 0x000000,
+    side: THREE.DoubleSide // TODO maybe not needed?
+  } );
+  var matLite = new THREE.MeshBasicMaterial( {
+    color: 0xFFD700,
+    transparent: true,
+    opacity: 0.4,
+    side: THREE.DoubleSide // TODO maybe not needed?
+  } );
+  return [matDark, matLite];
+}
+
 function main(rootEl) {
   const [w, h, renderer] = initRenderCanvas(rootEl);
   var camera, scene;
@@ -13,56 +65,27 @@ function main(rootEl) {
   controls.target.set( 0, 0, 0 );
   controls.update();
   scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0xf0f0f0 );
+  scene.background = new THREE.Color( 0xCCF2FF );
   var loader = new THREE.FontLoader();
   loader.load( '/fonts/helvetiker_regular.typeface.json', function ( font ) {
-    var xMid, text;
-    var textShape = new THREE.BufferGeometry();
-    var color = 0x006699;
-    var matDark = new THREE.LineBasicMaterial( {
-      color: color,
-      side: THREE.DoubleSide
-    } );
-    var matLite = new THREE.MeshBasicMaterial( {
-      color: color,
-      transparent: true,
-      opacity: 0.4,
-      side: THREE.DoubleSide
-    } );
-    var message = "Look\nOut!";
-    var shapes = font.generateShapes( message, 100, 2 );
-    var geometry = new THREE.ShapeGeometry( shapes );
-    geometry.computeBoundingBox();
-    xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
-    geometry.translate( xMid, 0, 0 );
-    // make shape ( N.B. edge view not visible )
-    textShape.fromGeometry( geometry );
-    text = new THREE.Mesh( textShape, matLite );
-    text.position.z = - 150;
-    scene.add( text );
-    // make line shape ( N.B. edge view remains visible )
-    var holeShapes = [];
-    for ( var i = 0; i < shapes.length; i ++ ) {
-      var shape = shapes[ i ];
-      if ( shape.holes && shape.holes.length > 0 ) {
-        for ( var j = 0; j < shape.holes.length; j ++ ) {
-          var hole = shape.holes[ j ];
-          holeShapes.push( hole );
-        }
-      }
-    }
-    shapes.push.apply( shapes, holeShapes );
-    var lineText = new THREE.Object3D();
-    for ( var i = 0; i < shapes.length; i ++ ) {
-      var shape = shapes[ i ];
-      var points = shape.getPoints();
-      var geometry = new THREE.BufferGeometry().setFromPoints( points );
+    var d = new Date();
+    var message = d.toLocaleDateString('en-US');
+    var [matDark, matLite] = initMaterials();
+    var [shapes, textShape, xMid] = initTextShape(font, message);
 
-      geometry.translate( xMid, 0, 0 );
-      var lineMesh = new THREE.Line( geometry, matDark );
-      lineText.add( lineMesh );
-    }
+    // make shape ( N.B. edge view not visible )
+    var text = new THREE.Mesh( textShape, matLite );
+    text.position.z = 5;
+    scene.add( text );
+
+    // make line shape ( N.B. edge view remains visible )
+    var holeShapes = initHoleShapes(shapes);
+    shapes.push.apply( shapes, holeShapes );
+
+    var lineText = initLineText(shapes, matDark, xMid);
     scene.add( lineText );
+
+
     animate();
     function animate() {
       requestAnimationFrame( animate );
