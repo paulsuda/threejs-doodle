3eb85349aaa85df38b2613aef317e8d2cf56377a
Paul Suda <paulsuda@gmail.com>
2018-01-05 21:27:35 -0600

    Moving module list out of main

--- src/script.js
+++ src/script.js
@@ -2,27 +2,8 @@
 const THREE = require('three');
 const { htmlMessage } = require('./shared/util');
 const marked = require('marked');
-
-const moduleList = [
-  require('./20180101-1/script'),
-  require('./20171229-1/script'),
-  require('./20171218-1/script'), // 15
-  require('./20171217-1/script'),
-  require('./20171215-1/script'),
-  require('./20171214-1/script'),
-  require('./20171208-1/script'),
-  require('./20171207-1/script'), // 10
-  require('./20171206-1/script'),
-  require('./20171202-3/script'),
-  require('./20171202-2/script'),
-  require('./20171202-1/script'),
-  require('./20171201-2/script'), // 5
-  require('./20171201-1/script'),
-  require('./20171129-1/script'),
-  require('./20171128-2/script'),
-  require('./20171128-1/script'), // 1
-  require('./stopped'),
-];
+const moduleList = require('./conf/moduleList');
+const { routeListenAndInit } = require('./shared/routes');
 
 function keyListen(callbackFn){
   document.addEventListener('keydown', (event) => {
