8217f6cd2563a689bb7a1ac4b3dbac05e7db6268
Paul Suda <paulsuda@gmail.com>
2018-12-01 20:28:46 -0600

    Cleaned up code

--- src/20180920-1/randomFill.glsl
+++ src/20180920-1/randomFill.glsl
@@ -2,19 +2,29 @@
 const float PI = 3.1415926535897932;
 uniform float rangeMin;
 uniform float rangeMax;
-vec2 mainSeeds = vec2(13.0, 22420.035);
+vec2 mainSeeds = vec2(72890.0, 27426.035);
+vec2 prngSeedOffsets = vec2(323.9, 999.3);
+vec2 primeMultipliers = vec2(11.0, 7.0);
 
-float seedRandom(float x, float seed){
-  float r = fract(sin(x * seed) + sin(x * 2355.0));
+float seedRandom(float x){
+  float r = fract(
+    sin(x * primeMultipliers.x + prngSeedOffsets.x) +
+    sin(x * primeMultipliers.y + prngSeedOffsets.y)
+  );
   return (r + rangeMin) * (rangeMax - rangeMin);
 }
 
 vec2 seedRandom2d(vec2 uv, vec2 seed){
-  return vec2(seedRandom(uv.x, uv.y), seedRandom(uv.y, uv.x));
+  float v = uv.x * uv.y;
+  return vec2(seedRandom(v * seed.x), seedRandom(v * seed.y));
+}
+
+void returnValue(vec4 r){
+  gl_FragColor = r;
 }
 
 void main() {
   vec2 uv = gl_FragCoord.xy / resolution.xy;
   vec2 r = seedRandom2d(uv, mainSeeds);
-  gl_FragColor = vec4(r.x, r.y, 0.0, 1.0);
+  returnValue(vec4(r.x, r.y, 0.0, 1.0));
 }
